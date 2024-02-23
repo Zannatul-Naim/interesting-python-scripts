0abee66fcdbc24dfd99cc50bc9920990b12e273e
Zannatul Naim <zannatulnaim09@gmail.com>
2024-02-24 04:41:45 +0600

    modified

--- Zip-Unzip-Files/zip.py
+++ Zip-Unzip-Files/zip.py
@@ -18,4 +18,4 @@ def zip_files_in_directory(source_directory, target_zip_file_path):
 # for example
 # zip_files_in_directory('C:/Users/HP/Desktop/fake', 'C:/Users/HP/Desktop/your_zip_file.zip') # use forward slash (/) or double backslash (\\)
 
-zip_files_in_directory('source_directory', 'target_directory/your_zip_file.zip')
+zip_files_in_directory('source_directory_path', 'target_directory/zip_file_name.zip')
